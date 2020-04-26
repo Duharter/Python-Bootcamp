c9b977cc274878bc1f5c6ee5adba3e4133b718fb
Duharter <duharter@ufl.edu>
2020-04-26 16:20:24 -0400

    First commit

---
+++ hangman/Hangman.py
@@ -0,0 +1,10 @@
+import time
+import random
+
+wordList = ["Chicken", "Paragraph", "Oliver", "Table", "Peperoni","Rockstar"]
+turns = 10
+guesses = ''
+
+word = random.choice(wordList)
+
+print(word)
